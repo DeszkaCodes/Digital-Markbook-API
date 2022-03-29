ea135331d0d7c5d4b5c38307b734aedf026a1726
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-03-29 17:47:51 +0200

    Updated Generator
    Updated Generator.cs to generate Teachers too

--- Generators/Generator.cs
+++ Generators/Generator.cs
@@ -10,16 +10,22 @@ public static class Generator
     public static School[] CurrentSchools { get; set; } = new School[0];
     public static Student[] CurrentStudents { get; set; } = new Student[0];
 
-    public static void StartGenerating(SchoolContext context, int studentAmount)
+    public static Teacher[] CurrentTeachers { get; set; } = new Teacher[0];
+
+    public static void StartGenerating(SchoolContext context, int studentAmount, int teacherAmount)
     {
         Schools();
         Student(studentAmount);
+        Teacher(teacherAmount);
 
         context.Schools
             .AddRange(CurrentSchools);
 
         context.Students
             .AddRange(CurrentStudents);
+
+        context.Teachers
+            .AddRange(CurrentTeachers);
 
         context.SaveChanges();
     }
@@ -82,6 +88,51 @@ public static class Generator
             var birthdate = new DateTime(year - rnd.Next(18,22), rnd.Next(1,13), rnd.Next(1,28));
 
             CurrentStudents[i] = new Student()
+                {
+                    Id = id,
+                    Name = name,
+                    Gender = isMale ? Gender.Male : Gender.Female,
+                    DateOfBirth = birthdate,
+                    School = CurrentSchools?.SelectRandom()
+                };
+        }
+    }
+
+    private static void Teacher(int amount)
+    {
+        CurrentTeachers = new Teacher[amount];
+
+        var rnd = new Random();
+
+        for(int i = 0; i < amount; i++)
+        {
+            var id = Guid.NewGuid();
+
+            var isMale = rnd.Next(0, 2) == 0 ? false : true;
+
+            var name = String.Empty;
+
+            if(isMale)
+            {
+                var datas = File.ReadAllLines("./Generators/malename.txt");
+
+                name = datas[rnd.Next(0, datas.Length)];
+            }
+            else
+            {
+                var datas = File.ReadAllLines("./Generators/femalename.txt");
+
+                name = datas[rnd.Next(0, datas.Length)];
+            }
+
+            var surnames = File.ReadAllLines("./Generators/surname.txt");
+            name += $" {surnames[rnd.Next(0, surnames.Length)]}";
+
+            var year = DateTime.Now.Year;
+
+            var birthdate = new DateTime(year - rnd.Next(26,60), rnd.Next(1,13), rnd.Next(1,28));
+
+            CurrentTeachers[i] = new Teacher()
                 {
                     Id = id,
                     Name = name,
