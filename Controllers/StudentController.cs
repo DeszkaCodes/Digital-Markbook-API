8b3b510140861c9751a6d9debea183dc99162220
Norbert Ormándi <60075235+Skydeszka@users.noreply.github.com>
2022-03-30 11:27:24 +0200

    Worked on readibility
    Changed some of the code for better readibility

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -23,10 +23,13 @@ public class StudentController : ControllerBase
     [HttpGet]
     public ActionResult<List<Student>> GetMany(int? limit)
     {
-        if(limit is not null && limit > 0)
-            return _service.GetAll().Take((int)limit).ToList();
-        
-        return _service.GetAll().ToList();
+        if(limit is null)
+            return _service.GetAll().ToList();
+            
+        if(limit <= 0)
+            return BadRequest();
+            
+        return _service.GetAll().Take((int)limit).ToList();
     }
 
     [HttpGet("{id}")]
