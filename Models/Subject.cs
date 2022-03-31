7b0a24147cd4bceaffec71e5b3fd14c3428c3fe2
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-03-31 22:44:50 +0200

    Create Subject.cs

---
+++ Models/Subject.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using SchoolAPI.Classes;
+
+namespace SchoolAPI.Models;
+
+public class Subject
+{
+    public Guid Id { get; set; }
+    
+    [Required]
+    public string? Name { get; set; }
+    
+    [Required]
+    public SubjectType Type { get; set; }
+
+    [Required]
+    public ICollection<Teacher>? Teachers { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Class>? Class { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Marks>? Marks { get; set; }
+}
