9430b9f71179c7020b37c3a15d4374b2c30927a0
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-04-02 17:02:07 +0200

    Added SelectManyRandom method

--- Classes/Extensioncs.cs
+++ Classes/Extensioncs.cs
@@ -4,6 +4,37 @@ namespace SchoolAPI.Classes;
 
 public static class Extensions
 {
+    public static IEnumerable<T> SelectManyRandom<T>(this IEnumerable<T> array, int amount)
+    {
+        bool Contains(T[] array, T item)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(EqualityComparer<T>.Default.Equals(array[i], item))
+                    return true;
+            }
+            return false;
+        }
+
+        T[] items = new T[amount];
+
+        for(int i = 0; i < amount; i++)
+        {
+            T item;
+            bool contains = false;
+            do
+            {
+                item = array.SelectRandom();
+
+                if(!Contains(items, item))
+                    contains = false;
+
+            } while(contains);
+        }
+
+        return items;
+    }
+
     public static T SelectRandom<T>(this IEnumerable<T> array)
     {
         var rnd = new Random();
