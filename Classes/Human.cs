3c1059c523d3a9f12c503e99f0dbef5e63c4648e
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-04-02 17:48:46 +0200

    Added constructor

--- Classes/Human.cs
+++ Classes/Human.cs
@@ -7,9 +7,16 @@ public class Human
     public Guid Id { get; set; }
 
     [Required]
-    public string? Name { get; set; }
+    public string Name { get; set; }
 
     [Required]
     public DateTime DateOfBirth { get; set; }
-    public Gender Gender { get; set; } = Gender.NotGiven;
+    public Gender Gender { get; set; }
+
+    public Human(Guid id, string name, DateTime dateofbirth, Gender gender = Gender.NotGiven)
+    {
+        Id = id;
+        DateOfBirth = dateofbirth;
+        Gender = gender;
+    }
 }
