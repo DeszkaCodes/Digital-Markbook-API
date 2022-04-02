163eb64afc1eb1dddda152511ca7de99f09d9358
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-04-02 18:44:27 +0200

    Removed constructor

--- Classes/Human.cs
+++ Classes/Human.cs
@@ -7,17 +7,9 @@ public class Human
     public Guid Id { get; set; }
 
     [Required]
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
     [Required]
     public DateTime DateOfBirth { get; set; }
-    public Gender Gender { get; set; }
-
-    public Human(Guid id, string name, DateTime dateofbirth, Gender gender = Gender.NotGiven)
-    {
-        Id = id;
-        Name = name;
-        DateOfBirth = dateofbirth;
-        Gender = gender;
-    }
+    public Gender Gender { get; set; } = Gender.NotGiven;
 }
