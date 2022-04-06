46bc6a7c3eb040ad9270152f0376d8fbd227402a
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-04-06 20:28:58 +0200

    Update SchoolClassService.cs

--- Services/SchoolClassService.cs
+++ Services/SchoolClassService.cs
@@ -15,17 +15,38 @@ public class SchoolClassService : IService<SchoolClass>
 
     public void Create(SchoolClass model)
     {
-        throw new NotImplementedException();
+        if(model.Name is null)
+            throw new ArgumentNullException("Name cannot be null");
+
+        if(model.HeadMaster is null)
+            throw new ArgumentNullException("HeadMaster cannot be null");
+
+        if(model.School is null)
+            throw new ArgumentNullException("School cannot be null");
+
+        _context.Classes.Add(model);
+
+        _context.SaveChanges();
     }
 
     public void Create(SchoolClass[] model)
     {
-        throw new NotImplementedException();
+        for(int i = 0; i < model.Length; i++)
+        {
+            Create(model[i]);
+        }
     }
 
     public void DeleteById(Guid id)
     {
-        throw new NotImplementedException();
+        var schoolClass = _context.Classes.Find(id);
+
+        if(schoolClass is null)
+            throw new Exception("School class not found");
+
+        _context.Classes.Remove(schoolClass);
+
+        _context.SaveChanges();
     }
 
     public IEnumerable<SchoolClass> GetAll()
@@ -53,6 +74,52 @@ public class SchoolClassService : IService<SchoolClass>
 
     public bool IdExists(Guid id)
     {
-        throw new NotImplementedException();
+        return _context.Classes.Find(id) is not null;
+    }
+
+    public void UpdateName(Guid id, string name)
+    {
+        var schoolClass = _context.Classes.Find(id);
+
+        if(schoolClass is null)
+            throw new NullReferenceException("School class does not exist");
+
+        schoolClass.Name = name;
+
+        _context.SaveChanges();
+    }
+
+    public void UpdateHeadmaster(Guid id, Guid teacherId)
+    {
+        var schoolClass = _context.Classes.Find(id);
+
+        if(schoolClass is null)
+            throw new NullReferenceException("School class does not exist");
+
+        var teacher = _context.Teachers.Find(teacherId);
+        
+        if(teacher is null)
+            throw new NullReferenceException("Teacher does not exist");
+
+        schoolClass.HeadMaster = teacher;
+
+        _context.SaveChanges();
+    }
+
+    public void UpdateSchool(Guid id, Guid schoolId)
+    {
+        var schoolClass = _context.Classes.Find(id);
+
+        if(schoolClass is null)
+            throw new NullReferenceException("School class does not exist");
+
+        var school = _context.Schools.Find(schoolId);
+        
+        if(school is null)
+            throw new NullReferenceException("School does not exist");
+
+        schoolClass.School = school;
+
+        _context.SaveChanges();
     }
 }
