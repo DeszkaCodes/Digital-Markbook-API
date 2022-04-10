9e84adbc954fb4da4c437e3134a834473b2ee718
skydeszka <60075235+Skydeszka@users.noreply.github.com>
2022-04-10 20:17:44 +0200

    Seperated by context

---
+++ Controllers/School/SchoolModifiers.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolAPI.Classes;
+using SchoolAPI.Models;
+using SchoolAPI.Services;
+
+namespace SchoolAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SchoolModifiers : ControllerBase
+{
+    private readonly SchoolService _service;
+
+    public SchoolModifiers(SchoolService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public ActionResult<School> Create([FromForm] string name, [FromForm] SchoolType type)
+    {
+        var school = new School()
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Type = type
+        };
+
+        _service.Create(school);
+
+        return CreatedAtAction(nameof(Create), school);
+    }
+
+    [HttpDelete("{id:Guid}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult Delete([FromRoute] Guid id)
+    {
+        var school = _service.GetById(id);
+
+        if(school is null)
+            return NotFound(new { error = "School not found" });
+
+        _service.DeleteById(id);
+
+        return Ok();
+    }
+
+    [HttpPatch("{id:Guid}/name")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult UpdateName([FromRoute] Guid id, [FromQuery] string name)
+    {
+        var school = _service.GetById(id);
+
+        if(school is null)
+            return NotFound(new { error = "School not found" });
+
+        _service.UpdateName(id, name);
+
+        return Ok();
+    }
+
+    [HttpPatch("{id:Guid}/type")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult UpdateType([FromRoute] Guid id, [FromQuery] SchoolType type)
+    {
+        var school = _service.GetById(id);
+
+        if(school is null)
+            return NotFound(new { error = "School not found" });
+
+        _service.UpdateType(id, type);
+
+        return Ok();
+    }
+}
--- Controllers/School/SchoolReaders.cs
+++ Controllers/School/SchoolReaders.cs
@@ -7,11 +7,11 @@ namespace SchoolAPI.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class SchoolController : ControllerBase
+public class SchoolReaders : ControllerBase
 {
     private SchoolService _service;
 
-    public SchoolController(SchoolService service)
+    public SchoolReaders(SchoolService service)
     {
         _service = service;
     }
@@ -56,52 +56,6 @@ public class SchoolController : ControllerBase
         return _service.GetTeachersById(id).ToList();
     }
 
-    [HttpPost]
-    [ProducesResponseType(StatusCodes.Status201Created)]
-    public ActionResult<School> Create([FromForm] string name, [FromForm] SchoolType type)
-    {
-        var school = new School()
-            {
-                Id = Guid.NewGuid(),
-                Name = name,
-                Type = type
-            };
-
-        _service.Create(school);
-
-        return CreatedAtAction(nameof(Create), school);
-    }
-
-    [HttpDelete("{id:Guid}")]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult Delete([FromRoute] Guid id)
-    {
-        var school = _service.GetById(id);
-
-        if(school is null)
-            return NotFound(new { error = "School not found" });
-
-        _service.DeleteById(id);
-
-        return Ok();
-    }
-
-    [HttpPatch("{id:Guid}/name")]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult UpdateName([FromRoute] Guid id, [FromQuery] string name)
-    {
-        var school = _service.GetById(id);
-
-        if(school is null)
-            return NotFound(new { error = "School not found" });
-
-        _service.UpdateName(id, name);
-
-        return Ok();
-    }
-
     [HttpGet("{id:Guid}/name")]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status200OK)]
@@ -113,21 +67,6 @@ public class SchoolController : ControllerBase
             return NotFound(new { error = "School not found" });
 
         return Ok(new { name = school.Name });
-    }
-
-    [HttpPatch("{id:Guid}/type")]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult UpdateType([FromRoute] Guid id, [FromQuery] SchoolType type)
-    {
-        var school = _service.GetById(id);
-
-        if(school is null)
-            return NotFound(new { error = "School not found" });
-
-        _service.UpdateType(id, type);
-
-        return Ok();
     }
 
     [HttpGet("{id:Guid}/type")]
